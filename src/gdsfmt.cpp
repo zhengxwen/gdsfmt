27a83f9e3014f6682461dad5f238de458967cc41
x.zheng <x.zheng@bc3139a8-67e5-0310-9ffc-ced21a209358>
2015-02-28 03:00:42 +0000

    Commit made by the Bioconductor Git-SVN bridge.
    Commit id: 6646acd4a99f763eca705999a98e5d2afa328ff9
    
        Oops, the bug in 'objdesp.gdsn' still exists, fixed!
    
    
    
    git-svn-id: https://hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/gdsfmt@99978 bc3139a8-67e5-0310-9ffc-ced21a209358

--- src/gdsfmt.cpp
+++ src/gdsfmt.cpp
@@ -1022,7 +1022,7 @@ COREARRAY_DLL_EXPORT SEXP gdsNodeObjDesp(SEXP Node)
 			{
 				PROTECT(tmp = NEW_LIST(1));
 				SEXP nm = PROTECT(NEW_STRING(1));
-				nProtected ++;
+				nProtected += 2;
 				SET_STRING_ELT(nm, 0, mkChar("maxlen"));
 				SET_NAMES(tmp, nm);
 
