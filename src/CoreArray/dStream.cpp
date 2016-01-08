405738e978d8d37bbfc39a5ebd81616d187256e5
Xiuwen Zheng <zhengxwen@gmail.com>
2016-01-07 23:01:57 -0800

    fix an issue of uninitialized value for LZ4 compression

--- src/CoreArray/dStream.cpp
+++ src/CoreArray/dStream.cpp
@@ -1494,6 +1494,7 @@ CdLZ4RA_Deflate::CdLZ4RA_Deflate(CdStream &Dest, TLevel Level,
 	{
 	case clFast:
 		fLZ4Ptr = malloc(sizeof(LZ4_stream_t));
+		memset(fLZ4Ptr, 0, sizeof(LZ4_stream_t));
 		break;
 	case clDefault: case clMax:
 		fLZ4Ptr = LZ4_createStreamHC();
@@ -1691,7 +1692,7 @@ CdLZ4RA_Inflate::CdLZ4RA_Inflate(CdStream &Source):
 	InitReadStream();
 	_IdxRaw = 1;
 	fCurPosition = 0;
-	lz4_body.table[0] = 0;
+	memset(&lz4_body, 0, sizeof(lz4_body));
 	iRaw = CntRaw = 0;
 }
 
@@ -1750,7 +1751,7 @@ ssize_t CdLZ4RA_Inflate::Read(void *Buffer, ssize_t Count)
 			{
 				// go to the next block
 				if (NextBlock())
-					lz4_body.table[0] = 0;
+					memset(&lz4_body, 0, sizeof(lz4_body));
 				else
 					break;
 			} else if (b > fCB_UZSize)
@@ -1783,7 +1784,7 @@ SIZE64 CdLZ4RA_Inflate::Seek(SIZE64 Offset, TdSysSeekOrg Origin)
 	bool flag = SeekStream(Offset);
 	if (flag || (Offset < fCurPosition))
 	{
-		lz4_body.table[0] = 0;
+		memset(&lz4_body, 0, sizeof(lz4_body));
 		iRaw = CntRaw = 0;
 		fStreamPos = fCB_ZStart + SIZE_RA_BLOCK_HEADER;
 		fCurPosition = fCB_UZStart;
