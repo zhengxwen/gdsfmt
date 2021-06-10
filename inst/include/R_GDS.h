8e3b9d140e3409f3862161a7fbb6419ed044eb42
Xiuwen Zheng <zhengxwen@gmail.com>
2021-06-10 17:37:15 -0500

    version bump

--- inst/include/R_GDS.h
+++ inst/include/R_GDS.h
@@ -71,8 +71,8 @@ extern "C" {
 
 	// ==================================================================
 
-	/// Version of R package gdsfmt: v1.27.8
-	#define GDSFMT_R_VERSION       0x011B08
+	/// Version of R package gdsfmt: v1.28.0
+	#define GDSFMT_R_VERSION       0x011C00
 
 
 	// [[ ********
