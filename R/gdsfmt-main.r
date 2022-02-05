99b971120cd8d3ea385b99e380dd8fea40d42c48
Xiuwen Zheng <zhengxwen@users.noreply.github.com>
2022-02-05 01:43:30 -0600

    remove duplicated

--- R/gdsfmt-main.r
+++ R/gdsfmt-main.r
@@ -23,14 +23,6 @@
 # File Operations
 ##############################################################################
 
-# return TRUE if file reopen occurs
-.reopen <- function(gdsfile)
-{
-    stopifnot(inherits(gdsfile, "gds.class"))
-    .Call(gdsReopenGDS, gdsfile)
-}
-
-
 #############################################################
 # Create a new CoreArray Genomic Data Structure (GDS) file
 #
