831722954c906c06b38a61be226fc9f65a4811af
Xiuwen Zheng <zhengxwen@users.noreply.github.com>
2026-05-24 13:23:35 -0500

    NEON for dBitGDS_Bit1.h

--- src/CoreArray/dBitGDS_Bit1.h
+++ src/CoreArray/dBitGDS_Bit1.h
@@ -8,7 +8,7 @@
 //
 // dBitGDS_Bit1.h: Bit operators and classes of GDS format for Bit1
 //
-// Copyright (C) 2007-2020    Xiuwen Zheng
+// Copyright (C) 2007-2026    Xiuwen Zheng
 //
 // This file is part of CoreArray.
 //
@@ -29,7 +29,7 @@
  *	\file     dBitGDS_Bit1.h
  *	\author   Xiuwen Zheng [zhengxwen@gmail.com]
  *	\version  1.0
- *	\date     2007 - 2020
+ *	\date     2007 - 2026
  *	\brief    Bit operators and classes of GDS format for Bit1
  *	\details
 **/
@@ -105,7 +105,7 @@ namespace CoreArray
 	};
 
 
-#ifdef COREARRAY_SIMD_SSE2
+#if defined(COREARRAY_SIMD_SSE2) || defined(COREARRAY_SIMD_NEON)
 
 	#define WRITE_BIT1_DECODE    \
 		{ \
@@ -139,6 +139,9 @@ namespace CoreArray
 			s += 8; \
 		}
 
+#endif
+
+#ifdef COREARRAY_SIMD_SSE2
 
 	static const __m128i BIT1_REP_x01 = _mm_set1_epi8(0x01);
 	static const __m128i BIT1_B4_x01 = _mm_set1_epi32(0x01);
@@ -312,6 +315,168 @@ namespace CoreArray
 				__m128i w2 = _mm_packs_epi32(v1, v2);
 				*((C_Int16*)p) = _mm_movemask_epi8(
 					_mm_slli_epi32(_mm_packus_epi16(w1, w2), 7));
+				p += 2; s += 16;
+			}
+			for (; n_byte > 0; n_byte--) WRITE_BIT1_ENCODE
+			return s;
+		}
+	};
+
+	template<> struct COREARRAY_DLL_LOCAL BIT1_CONV<C_UInt32>
+	{
+		inline static C_UInt32* Decode(const C_UInt8 *s, size_t n_byte, C_UInt32 *p)
+		{
+			return (C_UInt32*)BIT1_CONV<C_Int32>::Decode(s, n_byte, (C_Int32*)p);
+		}
+		inline static C_UInt32* Decode2(const C_UInt8 *s, size_t n_byte, C_UInt32 *p,
+			const C_BOOL sel[])
+		{
+			return (C_UInt32*)BIT1_CONV<C_Int32>::Decode2(s, n_byte, (C_Int32*)p, sel);
+		}
+		inline static const C_UInt32 *Encode(const C_UInt32 *s, C_UInt8 *p,
+			size_t n_byte)
+		{
+			return (C_UInt32*)BIT1_CONV<C_Int32>::Encode((C_Int32*)s, p, n_byte);
+		}
+	};
+
+#elif defined(COREARRAY_SIMD_NEON)
+
+	template<> struct COREARRAY_DLL_LOCAL BIT1_CONV<C_UInt8>
+	{
+		inline static C_UInt8* Decode(const C_UInt8 *s, size_t n_byte, C_UInt8 *p)
+		{
+			const uint8x16_t mask01 = vdupq_n_u8(0x01);
+			// pre-negated shift amounts: 0,-1,-2,-3,-4,-5,-6,-7 for each half
+			const int8x16_t neg_shifts = vreinterpretq_s8_u8(vcombine_u8(
+				vcreate_u8(0xF9FAFBFCFDFEFF00ULL),
+				vcreate_u8(0xF9FAFBFCFDFEFF00ULL)));
+			for (; n_byte >= 2; n_byte -= 2)
+			{
+				// Replicate each source byte into its own 8-byte half
+				uint8x16_t v = vcombine_u8(vdup_n_u8(s[0]), vdup_n_u8(s[1]));
+				s += 2;
+				// Right-shift each lane by its bit position and mask to bit 0
+				vst1q_u8(p, vandq_u8(vshlq_u8(v, neg_shifts), mask01));
+				p += 16;
+			}
+			for (; n_byte > 0; n_byte--) WRITE_BIT1_DECODE
+			return p;
+		}
+
+		inline static C_UInt8* Decode2(const C_UInt8 *s, size_t n_byte, C_UInt8 *p,
+			const C_BOOL sel[])
+		{
+			for (; n_byte > 0; n_byte--) WRITE_BIT1_SEL_DECODE
+			return p;
+		}
+
+		inline static const C_UInt8 *Encode(const C_UInt8 *s, C_UInt8 *p,
+			size_t n_byte)
+		{
+			for (; n_byte >= 2; n_byte -= 2)
+			{
+				// load 16 bytes, extract bit 0 of each into a 16-bit mask
+				uint8x16_t v = vld1q_u8(s);
+				// shift bit 0 to bit 7 position for each byte
+				uint8x16_t bits = vshlq_n_u8(v, 7);
+				// Use narrowing to pack: there's no movemask on NEON,
+				// so extract manually
+				uint8_t tmp[16];
+				vst1q_u8(tmp, bits);
+				C_UInt8 r0 = 0, r1 = 0;
+				for (int i = 0; i < 8; i++)
+					r0 |= ((tmp[i] >> 7) << i);
+				for (int i = 0; i < 8; i++)
+					r1 |= ((tmp[8+i] >> 7) << i);
+				p[0] = r0; p[1] = r1;
+				p += 2; s += 16;
+			}
+			for (; n_byte > 0; n_byte--) WRITE_BIT1_ENCODE
+			return s;
+		}
+	};
+
+	template<> struct COREARRAY_DLL_LOCAL BIT1_CONV<C_Int8>
+	{
+		inline static C_Int8* Decode(const C_UInt8 *s, size_t n_byte, C_Int8 *p)
+		{
+			return (C_Int8*)BIT1_CONV<C_UInt8>::Decode(s, n_byte, (C_UInt8*)p);
+		}
+		inline static C_Int8* Decode2(const C_UInt8 *s, size_t n_byte, C_Int8 *p,
+			const C_BOOL sel[])
+		{
+			return (C_Int8*)BIT1_CONV<C_UInt8>::Decode2(s, n_byte, (C_UInt8*)p, sel);
+		}
+		inline static const C_Int8 *Encode(const C_Int8 *s, C_UInt8 *p,
+			size_t n_byte)
+		{
+			return (C_Int8*)BIT1_CONV<C_UInt8>::Encode((C_UInt8*)s, p, n_byte);
+		}
+	};
+
+
+	// ===========================================================
+
+	template<> struct COREARRAY_DLL_LOCAL BIT1_CONV<C_Int32>
+	{
+		inline static C_Int32* Decode(const C_UInt8 *s, size_t n_byte, C_Int32 *p)
+		{
+			const uint32x4_t mask01_32 = vdupq_n_u32(0x01);
+			// pre-negated shift amounts: {0,-1,-2,-3} and {-4,-5,-6,-7}
+			const int32x4_t neg_sh_lo = vreinterpretq_s32_u64(vcombine_u64(
+				vcreate_u64(0xFFFFFFFF00000000ULL), vcreate_u64(0xFFFFFFFDFFFFFFFEULL)));
+			const int32x4_t neg_sh_hi = vreinterpretq_s32_u64(vcombine_u64(
+				vcreate_u64(0xFFFFFFFBFFFFFFFCULL), vcreate_u64(0xFFFFFFF9FFFFFFFAULL)));
+			for (; n_byte > 0; n_byte--)
+			{
+				uint32x4_t vb = vdupq_n_u32(*s++);
+				vst1q_u32((uint32_t*)p, vandq_u32(vshlq_u32(vb, neg_sh_lo), mask01_32));
+				vst1q_u32((uint32_t*)(p+4), vandq_u32(vshlq_u32(vb, neg_sh_hi), mask01_32));
+				p += 8;
+			}
+			return p;
+		}
+
+		inline static C_Int32* Decode2(const C_UInt8 *s, size_t n_byte, C_Int32 *p,
+			const C_BOOL sel[])
+		{
+			for (; n_byte > 0; n_byte--) WRITE_BIT1_SEL_DECODE
+			return p;
+		}
+
+		inline static const C_Int32 *Encode(const C_Int32 *s, C_UInt8 *p,
+			size_t n_byte)
+		{
+			const uint32x4_t mask01_32 = vdupq_n_u32(0x01);
+			for (; n_byte >= 2; n_byte -= 2)
+			{
+				// encode 16 int32 values -> 2 bytes
+				uint32x4_t q0 = vandq_u32(vld1q_u32((const uint32_t*)s), mask01_32);
+				uint32x4_t q1 = vandq_u32(vld1q_u32((const uint32_t*)(s+4)), mask01_32);
+				uint32x4_t q2 = vandq_u32(vld1q_u32((const uint32_t*)(s+8)), mask01_32);
+				uint32x4_t q3 = vandq_u32(vld1q_u32((const uint32_t*)(s+12)), mask01_32);
+				// narrow 32->16->8
+				uint16x4_t h0 = vmovn_u32(q0);
+				uint16x4_t h1 = vmovn_u32(q1);
+				uint16x4_t h2 = vmovn_u32(q2);
+				uint16x4_t h3 = vmovn_u32(q3);
+				uint16x8_t h01 = vcombine_u16(h0, h1);
+				uint16x8_t h23 = vcombine_u16(h2, h3);
+				uint8x8_t b01 = vmovn_u16(h01);
+				uint8x8_t b23 = vmovn_u16(h23);
+				uint8x16_t bytes = vcombine_u8(b01, b23);
+				// now bytes[i] is 0 or 1 for i=0..15
+				// shift bit 0 to bit 7, then extract
+				uint8x16_t bits = vshlq_n_u8(bytes, 7);
+				uint8_t tmp[16];
+				vst1q_u8(tmp, bits);
+				C_UInt8 r0 = 0, r1 = 0;
+				for (int i = 0; i < 8; i++)
+					r0 |= ((tmp[i] >> 7) << i);
+				for (int i = 0; i < 8; i++)
+					r1 |= ((tmp[8+i] >> 7) << i);
+				p[0] = r0; p[1] = r1;
 				p += 2; s += 16;
 			}
 			for (; n_byte > 0; n_byte--) WRITE_BIT1_ENCODE
